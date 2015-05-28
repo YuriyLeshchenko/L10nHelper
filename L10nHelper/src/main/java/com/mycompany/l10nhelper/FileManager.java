e4f15c4a999abbd4231f70d05bbb37fff15567b0
yuriy.l <yuriyleshchenko@gmail.com>
2015-05-28 16:18:00 +0300

    Other sort method

--- L10nHelper/src/main/java/com/mycompany/l10nhelper/FileManager.java
+++ L10nHelper/src/main/java/com/mycompany/l10nhelper/FileManager.java
@@ -1,17 +1,19 @@
 package com.mycompany.l10nhelper;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
-import java.util.Date;
-import java.util.Properties;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.Set;
+import java.util.TreeSet;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
 
 @Named
 @SessionScoped
@@ -24,35 +26,45 @@ public class FileManager implements Serializable {
     private StreamedContent russianFile;
     private StreamedContent arabicFile;
 
-    private StreamedContent prepareFileToDownload(String fileName, Properties languageFile) {
-        SortedMap sortedPropertiesMap = new TreeMap(languageFile);
-        StringBuilder content = new StringBuilder();
-        content.append("# Last date sorting: ").append(new Date()).append('\n');
-        for (Object key : sortedPropertiesMap.keySet()) {
-            content.append(key).append("=").append(sortedPropertiesMap.get(key)).append('\n');
+    private StreamedContent prepareSortedFile(String fileName, UploadedFile uploadedFile) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(uploadedFile.getInputstream()));
+            Set<String> sortedSet = new TreeSet<>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sortedSet.add(line);
+            }
+
+            StringBuilder content = new StringBuilder();
+            for (String keyValueString : sortedSet) {
+                content.append(keyValueString).append('\n');
+            }
+            InputStream stream = new ByteArrayInputStream(content.toString().getBytes());
+            StreamedContent file = new DefaultStreamedContent(stream, "text/plain", fileName);
+            return file;
+        } catch (IOException ex) {
+            System.out.println("Error in prepareSortedFile();");
         }
-        InputStream stream = new ByteArrayInputStream(content.toString().getBytes());
-        StreamedContent file = new DefaultStreamedContent(stream, "text/plain", fileName);
-        return file;
+        return null;
     }
 
     public StreamedContent getEnglishFile() {
-        englishFile = prepareFileToDownload(Language.ENGLISH_FILE_NAME, localizationTable.getFileOfEnglish().getLanguageFile());
+        englishFile = prepareSortedFile(Language.ENGLISH_FILE_NAME, localizationTable.getFileOfEnglish().getUploadedFile());
         return englishFile;
     }
 
     public StreamedContent getSpanishFile() {
-        spanishFile = prepareFileToDownload(Language.SPANISH_FILE_NAME, localizationTable.getFileOfSpanish().getLanguageFile());
+        spanishFile = prepareSortedFile(Language.SPANISH_FILE_NAME, localizationTable.getFileOfSpanish().getUploadedFile());
         return spanishFile;
     }
 
     public StreamedContent getRussianFile() {
-        russianFile = prepareFileToDownload(Language.RUSSIAN_FILE_NAME, localizationTable.getFileOfRussian().getLanguageFile());
+        russianFile = prepareSortedFile(Language.RUSSIAN_FILE_NAME, localizationTable.getFileOfRussian().getUploadedFile());
         return russianFile;
     }
 
     public StreamedContent getArabicFile() {
-        arabicFile = prepareFileToDownload(Language.ARABIC_FILE_NAME, localizationTable.getFileOfArabic().getLanguageFile());
+        arabicFile = prepareSortedFile(Language.ARABIC_FILE_NAME, localizationTable.getFileOfArabic().getUploadedFile());
         return arabicFile;
     }
 }
